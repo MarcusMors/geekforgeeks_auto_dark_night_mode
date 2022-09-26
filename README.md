bcddec086eda5090f5ec9e0d5350c061d8183ae7
Jose Vilca Campana @marcusmors <alivezeh@gmail.com>
2022-09-25 22:03:57 -0500

    :sparkles: it works, it sets geeksforgeeks to dark mode!

---
+++ README.md
@@ -0,0 +1,24 @@
+<!--
+ Copyright (C) 2022 José Enrique Vilca Campana
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+# Geeksforgeeks  Auto Dark Night mode
+
+This ViolentMonkey script was made with the purpose to let firefox users with
+disabled cookies to take care of their eyes.
+
+Geeksforgeeks theme settings are saved in cookies and not in the dark theme API
+
--- script.user.js
+++ script.user.js
@@ -1,26 +1,45 @@
 // ==UserScript==
-// @name        Geeksforgeeks auto dark_night mode - geeksforgeeks.org
-// @namespace   https://marcusmors.com
-// @match       https://www.geeksforgeeks.org
+// @name        Geeksforgeeks auto dark night mode - geeksforgeeks.org
+// @namespace   marcusmors.com
+// @match       https://www.geeksforgeeks.org/*
 // @grant       none
-// @version     0.1
-// @author      jose vilca <marcusmors>
-// @description Clicks Geekforgeeks dark theme button.
+// @version     1
+// @author      Jose Vilca <@marcusmors>
+// @description sets Geekforgeeks dark theme auto.
 // @icon        https://www.google.com/s2/favicons?domain=geeksforgeeks.org
 // @license     MIT
 // ==/UserScript==
 
-function button_click() {
-	// let page_button = document.querySelectorAll('[data-gfg-action="toggleGFGTheme"]')
-	let page_button = document.querySelector('[data-gfg-action="toggleGFGTheme"]')
-	// document.querySelectorAll('[data-gfg-action="toggleGFGTheme"]').forEach((el) => el.click())
-	page_button.click()
+// cookie options: gfgThemeLight or gfgThemeDark //last checked 25/09/2022
+
+function get_cookie_value(cookie_key) {
+	let name = cookie_key + "="
+	let ca = document.cookie.split(";")
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i].trim()
+		if (c.indexOf(name) == 0) {
+			return c.substr(name.length)
+		}
+	}
+	return null
+	// https://stackoverflow.com/questions/23641531/get-a-cookie-value-javascript/23642134#23642134
 }
 
-// data-gfg-action="toggleGFGTheme"
-// fav violent monkey theme settings
-// yonce
-// seti
-// ruby blue
-// monokai
-// material
+function set_to_dark() {
+	"use strict"
+	const theme = get_cookie_value("gfg_theme") === "gfgThemeDark" ? "dark" : "light"
+
+	if (theme === "dark") return
+
+	const menu = document.querySelector(".hamburger-menu")
+	if (menu) {
+		// click the dark mode button inside the menu.
+		menu.click()
+		document.querySelector(".toggle-darkMode").click()
+		menu.click()
+		return
+	}
+	document.querySelectorAll('[data-gfg-action="toggleGFGTheme"]')[1].click()
+}
+
+jQuery(document).ready(set_to_dark)
